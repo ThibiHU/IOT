0f10461e844954bd734517634a0cc87f50680e82
jellebecude <75610881+jellebecude@users.noreply.github.com>
2021-11-12 16:31:37 +0100

    Create slagboom_open.py

---
+++ slagboom_open.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as mqtt
+import json
+
+MQTT_host = "192.168.137.178"
+MQTT_port = 1883
+
+MQTT_channel = "domoticz/in"
+MQTT_message = json.dumps({"command": "switchlight", "idx": 3, "switchcmd": "On" })
+
+x = 0
+
+while x <= 10:
+# This is the Publisher
+    client = mqtt.Client()
+    client.connect(MQTT_host,MQTT_port,60)
+    client.publish(MQTT_channel, MQTT_message)
+    client.disconnect()
+
+    x += 1
+    print(x)
